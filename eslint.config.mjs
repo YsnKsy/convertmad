1853a02a2ec4c9d6f6919a771ba68352a2059103
Yassine Kassy <5409223+YsnKsy@users.noreply.github.com>
2025-03-30 20:09:28 -0400

    feat: Mise à jour de la configuration ESLint et ajout de CurrencyUnit à l'export

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,12 +12,13 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([globalIgnores([
+export default defineConfig([
+  globalIgnores([
     "**/node_modules/",
     "**/dist/",
     "**/build/",
@@ -30,37 +31,46 @@ export default defineConfig([globalIgnores([
     "**/*.local",
     "**/*.tmp",
     "**/*.bak",
-]), {
-    extends: fixupConfigRules(compat.extends(
+    "**/*.npmrc",
+    "**/*.npmignore",
+  ]),
+  {
+    extends: fixupConfigRules(
+      compat.extends(
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:import/recommended",
         "plugin:import/typescript",
-        "plugin:prettier/recommended",
-    )),
+        "plugin:prettier/recommended"
+      )
+    ),
 
     plugins: {
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
-        import: fixupPluginRules(_import),
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      import: fixupPluginRules(_import),
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.commonjs,
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.commonjs,
+      },
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
 
     rules: {
-        "@typescript-eslint/no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
-        "no-console": "warn",
-        "import/no-unresolved": "error",
-        // semi: ["error", "never"],
-        // quotes: ["error", "single"],
-        "prettier/prettier": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { vars: "all", args: "after-used", ignoreRestSiblings: false },
+      ],
+      "no-console": "warn",
+      "import/no-unresolved": "error",
+      // semi: ["error", "never"],
+      // quotes: ["error", "single"],
+      "prettier/prettier": "error",
     },
-}]);
+  },
+]);
